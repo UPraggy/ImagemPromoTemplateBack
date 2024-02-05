aeb264ba4d94bdd475b3d696fa9d36b1c244b117
unknown <led.office.mail@gmail.com>
2024-02-05 11:52:24 -0300

    Adicionando mais revistas ao select

--- src/controllers/ConsultaColecoesRevistas.js
+++ src/controllers/ConsultaColecoesRevistas.js
@@ -3,10 +3,16 @@ const db = require("../config/databasecloud");
 exports.REST = async (req, res) => {
     try {
         const result = await db.query(`
-        select rev_num_rev ,rev_nom
-        from erp_revistas
-        where rev_status = 1 and rev_num_rev > 15
-        order by rev_datinc desc` );
+        (select rev_num_rev ,rev_nom
+            from erp_revistas
+            where  rev_num_rev > 10080 and rev_num_rev < 11000
+    order by rev_datinc desc)
+    UNION ALL
+    (select rev_num_rev ,rev_nom
+            from erp_revistas
+            where  rev_num_rev > 200 and rev_num_rev < 10000
+    order by rev_datinc desc)
+    ` );
 
         const rows = result.rows;
 
